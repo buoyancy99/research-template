a031a763e480e01d8f00576f9f024f1d7756d007
Boyuan Chen <boyuanchen0@gmail.com>
2024-11-11 22:32:59 -0500

    is_rank_zero

--- utils/distributed_utils.py
+++ utils/distributed_utils.py
@@ -1,3 +1,8 @@
-import wandb
+from typing import Callable
+import torch
+import torch.distributed as dist
+from lightning.pytorch.utilities.rank_zero import rank_zero_only
 
-is_rank_zero = wandb.run is not None
+is_rank_zero = rank_zero_only.rank == 0
+
+rank_zero_print = rank_zero_only(print)
